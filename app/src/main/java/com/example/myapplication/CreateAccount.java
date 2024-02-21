83e608fd682badf946caaedb062c751e13cdafd4
Phillip <ochiengphillip84@gmail.com>
2024-02-21 09:32:19 +0300

    ✨Feat: Add a recycler view to the homepage activity

--- app/src/main/java/com/example/myapplication/CreateAccount.java
+++ app/src/main/java/com/example/myapplication/CreateAccount.java
@@ -93,6 +93,5 @@ public class CreateAccount extends AppCompatActivity {
 
 
 
-
     }
 }
--- app/src/main/java/com/example/myapplication/HomeActivity.java
+++ app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -3,12 +3,14 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.widget.Button;
 public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
     }
 }
